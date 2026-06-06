dc20b7970605f6ae2683c02527cb2326f89638bd
Hosein-Bakhshayesh <hoseinb2.0181@gmail.com>
2026-06-06 21:48:26 +0330

    Complete GiftCodes

---
+++ Vitorize/Vitorize.Web/Models/Admin/GiftCodes/GiftCodeBatchModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Vitorize.Web.Models.Admin.GiftCodes
+{
+    public class GiftCodeBatchModel
+    {
+        public Guid Id { get; set; }
+
+        public string BatchTitle { get; set; } = string.Empty;
+
+        public string? SourceName { get; set; }
+
+        public int TotalCodes { get; set; }
+
+        public int AvailableCodes { get; set; }
+
+        public DateTime ImportedAt { get; set; }
+
+        public int SoldCodes => TotalCodes - AvailableCodes;
+    }
+}
